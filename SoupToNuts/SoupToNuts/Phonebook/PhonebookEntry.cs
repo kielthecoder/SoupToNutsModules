bfc6139737115d96a7840bb5f9bdbfab4cf26b14
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-10-28 16:47:29 -0400

    Created PhonebookEntry class

---
+++ SoupToNuts/SoupToNuts/Phonebook/PhonebookEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SoupToNuts.Phonebook
+{
+    public class PhonebookEntry
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ SoupToNuts/SoupToNuts/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SoupToNuts")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoupToNuts")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
