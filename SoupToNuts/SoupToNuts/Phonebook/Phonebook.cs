25faac3ba1072fc87fe965ab333093fe1c1e6133
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-10-28 17:32:31 -0400

    Created Phonebook class

---
+++ SoupToNuts/SoupToNuts/Phonebook/Phonebook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+
+namespace SoupToNuts.Phonebook
+{
+    public class Phonebook
+    {
+        private List<PhonebookEntry> _entries;
+        private string _filename;
+
+        public Phonebook()
+        {
+            _entries = new List<PhonebookEntry>();
+        }
+
+        public void Initialize(string filename)
+        {
+            _filename = filename;
+            
+            _entries.Clear();
+
+            using (var stream = File.OpenText(_filename))
+            {
+                while (!stream.EndOfStream)
+                {
+                    var text = stream.ReadLine();
+
+                    if (text.IndexOf('|') > 0)
+                    {
+                        var fields = text.Split('|');
+                        _entries.Add(new PhonebookEntry {
+                            Name = fields[0],
+                            Number = fields[1]
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
