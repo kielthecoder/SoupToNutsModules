a139b9daf3ecda0af57624ec58d0a968f6f2f312
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-11-13 10:39:52 -0500

    added IComparable to PhonebookEntry

--- SoupToNuts/SoupToNuts/Phonebook/PhonebookEntry.cs
+++ SoupToNuts/SoupToNuts/Phonebook/PhonebookEntry.cs
@@ -2,9 +2,17 @@
 
 namespace SoupToNuts.Phonebook
 {
-    public class PhonebookEntry
+    public class PhonebookEntry : IComparable<PhonebookEntry>
     {
         public string Name { get; set; }
         public string Number { get; set; }
+
+        public int CompareTo(PhonebookEntry other)
+        {
+            if (other == null)
+                return 1;
+
+            return this.Name.CompareTo(other.Name);
+        }
     }
 }
